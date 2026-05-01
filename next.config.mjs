fb4c0759ea8815539ad6966098f172a7f5e19d1f
Md Ashikur Rahman <ashikusa360@gmail.com>
2026-05-01 06:10:52 +0600

    Add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.myshopify.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
+};
+
+export default withNextIntl(nextConfig);
